a589650d2fcb8628ec2e12b02624cb87d7d1a60d
Jesús García <caballero.plz@gmail.com>
2023-10-08 16:45:21 +0200

    add Beer to domain

---
+++ src/Domain/Beer/Beer.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Domain\Beer;
+
+use App\Domain\Beer\ValueObject\BeerId;
+use App\Domain\Beer\ValueObject\TagLine;
+use App\Domain\Beer\ValueObject\Name;
+use App\Domain\Beer\ValueObject\ImageUrl;
+use App\Domain\Beer\ValueObject\Description;
+use App\Domain\Beer\ValueObject\FirstBrewed;
+
+class Beer
+{
+    private BeerId $id;
+    private Name $name;
+    private TagLine $tagline;
+    private FirstBrewed $firstBrewed;
+    private Description $description;
+    private ImageUrl $imageUrl;
+
+    public function __construct(
+        int $id,
+        string $name,
+        ?string $tagline,
+        ?string $firstBrewed,
+        ?string $description,
+        ?string $imageUrl
+    ) {
+        $this->id = new BeerId($id);
+        $this->name = new Name($name);
+        $this->tagline = new TagLine($tagline);
+        $this->firstBrewed = new FirstBrewed($firstBrewed);
+        $this->description = new Description($description);
+        $this->imageUrl = new ImageUrl($imageUrl);
+    }
+
+
+    public function getId(): BeerId
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): void
+    {
+        $this->id = $id;
+    }
+
+    public function getName(): Name
+    {
+        return $this->name;
+    }
+
+    
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getTagline(): ?TagLine
+    {
+        return $this->tagline;
+    }
+
+    public function setTagline(string $tagline): void
+    {
+        $this->tagline = $tagline;
+    }
+
+    public function getFirstBrewed(): ?FirstBrewed
+    {
+        return $this->firstBrewed;
+    }
+
+    public function setFirstBrewed(string $firstBrewed): void
+    {
+        $this->firstBrewed = $firstBrewed;
+    }
+
+    public function getDescription(): ?Description
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): void
+    {
+        $this->description = $description;
+    }
+
+    public function getImageUrl(): ?ImageUrl
+    {
+        return $this->imageUrl;
+    }
+
+    public function setImageUrl(?string $imageUrl): void
+    {
+        $this->imageUrl = $imageUrl;
+    }
+
+    public function toArray(): array
+    {
+        return [
+            'id' => $this->getId()->getValue(),
+            'name' => $this->getName()->getValue(),
+            'tagline' => $this->getTagline()->getValue(),
+            'firstBrewed' => $this->getFirstBrewed()->getValue(),
+            'description' => $this->getDescription()->getValue(),
+            'imageUrl' => $this->getImageUrl()->getValue()
+        ];
+    }
+
+}
